0dd3d6db1ff461852d8212c251a3ca2a99822ff9
hshun37 <hshun37@gmail.com>
2022-02-13 17:30:14 +0900

    공통 모듈 분리

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,23 +1,3 @@
-/* 헤더 검색창 파트 */
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
-
 /* 배지 보이기 숨기기 파트 */
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -154,10 +134,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-/* FOOTER부분 자동 년도 생성 */
-const thisYear = document.querySelector('.this-year');
-// textContent 메소드는 그 요소가 가지고 있는 글자 내용들의 값들을 알아내거 나 값을 지정할 수 있다.
-// 자바스크립드에서는 현재 날짜 정보를 가지고 있는 new Date();라는 객체를 가지고 있다.
-thisYear.textContent = new Date().getFullYear(); 
